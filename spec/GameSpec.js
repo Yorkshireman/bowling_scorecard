2764e35f32995e9c0af794b765f9a82bfc017a39
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-08-31 19:29:42 +0100

    first simple score test passed

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -7,26 +7,14 @@ describe("Game", function() {
   });
 
   describe("when game starts", function() {
-    it('the game is on frame 1', function() {
-      expect(game.frame).toEqual(1);
-    });
-
     describe("when player scores a 3 and a 6", function() {
       beforeEach(function() {
         game.bowl(3);
         game.bowl(6);
       });
 
-      it("the current finalScore is 9", function() {
-        expect(game.finalScore).toEqual(9);
-      })
-
-      it("the current frame is 2", function() {
-        expect(game.frame).toEqual(2);
-      })
-
-      it("strike status is null", function() {
-        expect(game.strike).toEqual(null);
+      it("the current totalScore is 9", function() {
+        expect(game.scoresArray).toEqual([3, 6]);
       })
     });
 
@@ -38,8 +26,8 @@ describe("Game", function() {
         game.bowl(4);
       });
 
-      it("the current finalScore is 20", function() {
-        expect(game.finalScore).toEqual(20);
+      it("the current totalScore is 20", function() {
+        expect(game.totalScore).toEqual(20);
       });
     });
 
@@ -51,12 +39,8 @@ describe("Game", function() {
         game.bowl(0);
       });
 
-      it("the current finalScore is 57", function() {
-        expect(game.finalScore).toEqual(57);
-      });
-
-      it("the current frame is 4", function() {
-        expect(game.frame).toEqual(4);
+      it("the current totalScore is 57", function() {
+        expect(game.totalScore).toEqual(57);
       });
 
       describe("if player then scores a 3 and a 5", function() {
@@ -65,8 +49,8 @@ describe("Game", function() {
           game.bowl(5)
         })
 
-        it("the current finalScore is 65", function() {
-          expect(game.finalScore).toEqual(65);
+        it("the current totalScore is 65", function() {
+          expect(game.totalScore).toEqual(65);
         });
       });
     });
@@ -80,102 +64,102 @@ describe("Game", function() {
         game.bowl(9);
       });
 
-      it("the current finalScore is 78", function() {
-        expect(game.finalScore).toEqual(78);
+      it("the current totalScore is 78", function() {
+        expect(game.totalScore).toEqual(78);
       })
     });
   });
 
-  describe("when in final frame", function() {
-    beforeEach(function() {
-      game.framesLeft = 1;
-      game.frame = 10;
-    });
+  // describe("when in tenth frame", function() {
+  //   beforeEach(function() {
+  //     game.framesLeft = 1;
+  //     game.frame = 10;
+  //   });
     
-    describe("with no strikes or spares left to add on", function() {
-      describe("when bowling a total of 5 points in that frame", function() {
-        beforeEach(function() {
-          game.bowl(3);
-          game.bowl(2);
-        });
+  //   describe("with no strikes or spares left to add on", function() {
+  //     describe("when bowling a total of 5 points in that frame", function() {
+  //       beforeEach(function() {
+  //         game.bowl(3);
+  //         game.bowl(2);
+  //       });
 
-        it('the game is correctly scored', function() {
-          expect(game.finalScore).toEqual(5);
-        });
+  //       it('the game is correctly scored', function() {
+  //         expect(game.finalScore).toEqual(5);
+  //       });
 
-        it("no more balls can be played", function() {
-          game.bowl(2);
-          expect(game.finalScore).toEqual(5);
-        });
-      });
+  //       it("no more balls can be played", function() {
+  //         game.bowl(2);
+  //         expect(game.finalScore).toEqual(5);
+  //       });
+  //     });
 
-      describe("when a spare is achieved", function() {
-        beforeEach(function() {
-          game.bowl(6);
-          game.bowl(4);
-        });
+  //     describe("when a spare is achieved", function() {
+  //       beforeEach(function() {
+  //         game.bowl(6);
+  //         game.bowl(4);
+  //       });
 
-        it("the game is on frame 11", function() {
-          expect(game.frame).toEqual(11);
-        })
+  //       it("the game is on frame 11", function() {
+  //         expect(game.frame).toEqual(11);
+  //       })
 
-        it("there is 1 ball left to play", function() {
-          expect(game.bowlsLeftInFrame).toEqual(1);
-        })
+  //       it("there is 1 ball left to play", function() {
+  //         expect(game.bowlsLeftInFrame).toEqual(1);
+  //       })
 
-        it("the current finalScore is 10", function() {
-          expect(game.finalScore).toEqual(10);
-        })
+  //       it("the current finalScore is 10", function() {
+  //         expect(game.finalScore).toEqual(10);
+  //       })
 
-        describe("if the player then bowls an 8", function() {
-          beforeEach(function() {
-            game.bowl(8);
-          });
+  //       describe("if the player then bowls an 8", function() {
+  //         beforeEach(function() {
+  //           game.bowl(8);
+  //         });
 
-          it("the final score is 18", function() {
-            expect(game.finalScore).toEqual(18);
-          })
+  //         it("the final score is 18", function() {
+  //           expect(game.finalScore).toEqual(18);
+  //         })
 
-          it("cannot score anymore points", function() {
-            game.bowl(3);
-            expect(game.finalScore).toEqual(18);
-          });
-        });
-      });
+  //         it("cannot score anymore points", function() {
+  //           game.bowl(3);
+  //           expect(game.finalScore).toEqual(18);
+  //         });
+  //       });
+  //     });
 
-      describe("when first ball bowls a strike", function() {
-        beforeEach(function() {
-          game.bowl(10);
-        });
+  //     describe("when first ball bowls a strike", function() {
+  //       beforeEach(function() {
+  //         game.bowl(10);
+  //       });
 
-        it('the game is on frame 11', function() {
-          expect(game.frame).toEqual(11);
-        });
+  //       it('the game is on frame 11', function() {
+  //         expect(game.frame).toEqual(11);
+  //       });
 
-        it("there are two balls left to play", function() {
-          expect(game.bowlsLeftInFrame).toEqual(2);
-        });
+  //       it("there are two balls left to play", function() {
+  //         expect(game.bowlsLeftInFrame).toEqual(2);
+  //       });
 
-        it("the current finalScore is 10", function() {
-          expect(game.finalScore).toEqual(10);
-        });
+  //       it("the current finalScore is 10", function() {
+  //         expect(game.finalScore).toEqual(10);
+  //       });
 
-        describe("if player then bowls a 3 and 6", function() {
-          beforeEach(function() {
-            game.bowl(3);
-            game.bowl(6);
-          });
+  //       describe("if player then bowls a 3 and 6", function() {
+  //         beforeEach(function() {
+  //           game.bowl(3);
+  //           game.bowl(6);
+  //         });
 
-          it("the finalScore is 28", function() {
-            expect(game.finalScore).toEqual(28);
-          });
+  //         it("the finalScore is 28", function() {
+  //           expect(game.finalScore).toEqual(28);
+  //         });
 
-          it("cannot score anymore points", function() {
-            game.bowl(4);
-            expect(game.finalScore).toEqual(28);
-          });
-        })
-      });
-    });
-  });
+  //         it("cannot score anymore points", function() {
+  //           game.bowl(4);
+  //           expect(game.finalScore).toEqual(28);
+  //         });
+  //       })
+  //     });
+  //   });
+  // });
 });
